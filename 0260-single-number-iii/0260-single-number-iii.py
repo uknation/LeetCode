7e0471a799cccdb184174839c7e5f108caac9a30
Yukesh Choudhary <157104816+uknation@users.noreply.github.com>
2026-06-29 18:03:27 +0530

    Time: 2 ms (75.26%), Space: 21.2 MB (11.38%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        eor = 0
+        for x in nums:
+            eor ^= x
+        lowbit = eor & (-eor)
+        ans = [0, 0]
+        for x in nums:
+            if (x & lowbit) == 0:
+                ans[0] ^= x
+            else:
+                ans[1] ^= x
+        return ans
+
+############
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xs = reduce(xor, nums)
+        a = 0
+        lb = xs & -xs
+        for x in nums:
+            if x & lb:
+                a ^= x
+        b = xs ^ a
+        return [a, b]

0e0176849d2149793d7bcdf7d2fee6a5b3727c5a
Yukesh Choudhary <157104816+uknation@users.noreply.github.com>
2026-07-24 16:11:16 +0530

    Time: 44 ms (26.82%), Space: 23.4 MB (34.9%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.py
@@ -0,0 +1,63 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    def getRandom(self) -> int:
+        n = ans = 0
+        head = self.head
+        while head:
+            n += 1
+            x = random.randint(1, n)
+            if n == x:
+                ans = head.val
+            head = head.next
+        return ans
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
+
+############
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+import random
+
+
+class Solution(object):
+
+  def __init__(self, head):
+    """
+    @param head The linked list's head.
+    Note that the head is guaranteed to be not null, so it contains at least one node.
+    :type head: ListNode
+    """
+    self.head = head
+
+  def getRandom(self):
+    """
+    Returns a random node's value.
+    :rtype: int
+    """
+    ans = self.head.val
+    head = self.head
+    idx = 1
+    while head:
+      if random.randrange(1, idx + 1) == idx:
+        ans = head.val
+      head = head.next
+      idx += 1
+    return ans
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
